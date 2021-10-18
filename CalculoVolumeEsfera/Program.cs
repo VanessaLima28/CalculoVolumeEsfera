e7ef2c6897429593f30b541c2e43cb6145ee0fc9
vanessa.vml.vm@hotmail.com <vanessa.vml.vm@hotmail.com>
2021-10-18 19:23:54 -0300

    adding files

---
+++ CalculoVolumeEsfera/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalculoVolumeEsfera
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double volume, raio;
+            Program program = new Program();
+
+            Console.WriteLine("Digite o valor do Raio:");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            volume = program.Calculo(raio);
+            Console.WriteLine("O volume da esfera é {0}: ", volume);
+        }
+        public double Calculo(double raio)
+        {
+            return  4 * 3.14 * Math.Pow (raio, 3) / 3;
+        }
+    }
+}
